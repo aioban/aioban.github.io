30343f45085aa227e7a810e79e1a00604a2f8a87
aioban <compagnucci.ja@gmail.com>
2025-08-09 21:49:46 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,12 @@ module.exports = {
   theme: {
     extend: {}
   },
-  safelist: [
+ safelist: [
+  "hidden","opacity-60","pointer-events-none","select-none","dark",
+  "bg-white/80","dark:bg-slate-900/70","border","border-slate-200","dark:border-slate-800",
+  "text-slate-600","dark:text-slate-400","dark:bg-slate-950","dark:text-slate-100"
+],
+
     // Clases que se agregan desde JS en runtime
     "hidden",
     "opacity-60",
